16b204ab1391adb78417ba69b6019eaadc297f39
Héctor Guerra <hectorluengo.xx@gmail.com>
2024-03-02 20:00:11 +0100

    intentando que lea utils.ts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  i18n: {
+    defaultLocale: "es",
+    locales: ["en", "es", "pt-br"],
+  },
+  routing: {
+    prefixDefaultLocale: false,
+  },
+  integrations: [react()],
 });
---
+++ src/i18n/ui.ts
@@ -0,0 +1,26 @@
+export const languages = {
+  es: 'Español',
+  en: 'English',
+};
+
+export const defaultLang = 'es';
+
+export const ui = {
+  es: {
+    'section.about': 'Sobre mi',
+    'section.education': 'Formación',
+    'section.experience': 'Experiencia Laboral',
+    'section.languages': 'Idioma',
+    'section.options': 'Opciones',
+    'section.projects': 'Proyectos',
+    
+  },
+  en: {
+    'section.about': 'About',
+    'section.education': 'Education',
+    'section.experience': 'Work Experience',
+    'section.languages': 'Languages',
+    'section.options': 'Options',
+    'section.projects': 'Projects',
+  },
+} as const;
