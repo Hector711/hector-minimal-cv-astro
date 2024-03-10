7681e2c55796e7823c3737daca7cef82aba5324a
Héctor Guerra <hectorluengo.xx@gmail.com>
2024-03-11 00:46:59 +0100

    tail first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,19 @@
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   i18n: {
     defaultLocale: "es",
-    locales: ["en", "es", "pt-br"],
+    locales: ["en", "es", "pt-br"]
   },
   routing: {
-    prefixDefaultLocale: false,
+    prefixDefaultLocale: false
   },
   fallback: {
     fr: "es"
   },
-  integrations: [react()],
+  integrations: [react(), tailwind()]
 });
-
-
